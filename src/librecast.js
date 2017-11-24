c7040f3260e97ef6aee15058bfe41efd2b9de64e
Brett Sheffield <brett@gladserv.com>
2017-11-24 09:54:33 +0000

    cleanup & lint

--- src/librecast.js
+++ src/librecast.js
@@ -21,12 +21,431 @@
  */
 
 var LIBRECAST = (function ($) {
-	"use strict";
-	var lc = {};
 
-	lc.init = function () {
-		console.log("init()");
+"use strict";
+var lc = {};
+
+var WS_CONNECTING = 0;
+var WS_OPEN = 1;
+var WS_CLOSING = 2;
+var WS_CLOSED = 3;
+
+var LC_ERROR_SUCCESS = 0;
+var LC_ERROR_FAILURE = 1;
+var LC_ERROR_WEBSOCKET_UNSUPPORTED = 2;
+var LC_ERROR_WEBSOCKET_NOTREADY = 3;
+var LC_ERROR_CALLBACK_NOT_FUNCTION = 4;
+
+var LCAST_OP_NOOP			  = 0x01;
+var LCAST_OP_SETOPT			  = 0x02;
+var LCAST_OP_SOCKET_NEW		  = 0x03;
+var LCAST_OP_SOCKET_GETOPT	  = 0x04;
+var LCAST_OP_SOCKET_SETOPT	  = 0x05;
+var LCAST_OP_SOCKET_LISTEN	  = 0x06;
+var LCAST_OP_SOCKET_IGNORE	  = 0x07;
+var LCAST_OP_SOCKET_CLOSE	  = 0x08;
+var LCAST_OP_SOCKET_MSG		  = 0x09;
+var LCAST_OP_CHANNEL_NEW	  = 0x0a;
+var LCAST_OP_CHANNEL_GETMSG   = 0x0b;
+var LCAST_OP_CHANNEL_GETOPT   = 0x0c;
+var LCAST_OP_CHANNEL_SETOPT   = 0x0d;
+var LCAST_OP_CHANNEL_GETVAL   = 0x0e;
+var LCAST_OP_CHANNEL_SETVAL   = 0x0f;
+var LCAST_OP_CHANNEL_BIND	  = 0x10;
+var LCAST_OP_CHANNEL_UNBIND   = 0x11;
+var LCAST_OP_CHANNEL_JOIN	  = 0x12;
+var LCAST_OP_CHANNEL_PART	  = 0x13;
+var LCAST_OP_CHANNEL_SEND	  = 0x14;
+
+var LCAST_HEADER_LENGTH = 25;
+
+var librecastErrorMsg = {};
+librecastErrorMsg[LC_ERROR_SUCCESS] = "Success";
+librecastErrorMsg[LC_ERROR_FAILURE] = "Failure";
+librecastErrorMsg[LC_ERROR_WEBSOCKET_UNSUPPORTED] = "Browser does not support websockets";
+librecastErrorMsg[LC_ERROR_WEBSOCKET_NOTREADY] = "Websocket not ready";
+librecastErrorMsg[LC_ERROR_CALLBACK_NOT_FUNCTION] = "Callback not a function";
+
+var tok = 42;
+var lcastCallbacks = {};
+var HAS_JQUERY = (typeof jQuery !== "undefined");
+
+
+/* convert utf16 to utf8 and append to dataview
+ * idx:		 starting byte in dataview to write to
+ * utf16in:  utf16 input
+ * len:		 length (characters) of utf16in
+ * dataview: DataView array to write to
+ * returns: index of last byte written in dataview */
+function convertUTF16toUTF8(idx, utf16in, len, dataview) {
+	var c, i;
+	for (i = 0; i < len; i++) {
+		c = utf16in.charCodeAt(i);
+		if (c <= 0x7f) {
+			dataview.setUint8(idx++, c);
+		}
+		else if (c <= 0x7ff) {
+			dataview.setUint8(idx++, 0xc0 | (c >>> 6));
+			dataview.setUint8(idx++, 0x80 | (c & 0x3f));
+		}
+		else if (c <= 0xffff) {
+			dataview.setUint8(idx++, 0xe0 | (c >>> 12));
+			dataview.setUint8(idx++, 0x80 | ((c >>> 6) & 0x3f));
+			dataview.setUint8(idx++, 0x80 | (c & 0x3f));
+		}
+		else {
+			console.log("UTF-16 surrogate pair, ignoring");
+			/* TODO: 4 byte UTF-8 encoding, just in case anyone speaks Vogon */
+		}
+	}
+	return idx;
+}
+
+
+/* return deferred if jQuery available */
+function defer() {
+	if (HAS_JQUERY) {
+		console.log("jQuery available, returning deferred");
+		try {
+			return $.Deferred();
+		}
+		catch(e) {
+			console.log("unable to set deferred");
+		}
+	}
+}
+
+
+/* extend DataView to provide missing getUint64() */
+/* this function based on code from spice-html5
+ * https://github.com/SPICE/spice-html5/blob/master/spicedataview.js
+ * Copyright (C) 2013 by Jeremy P. White <jwhite@codeweavers.com>
+ * License: LGPL3+ */
+if (DataView.prototype.getUint64 === undefined) {
+	DataView.prototype.getUint64 = function (byteOffset, littleEndian) {
+		var lo = (littleEndian) ? 0 : 4;
+		var hi = (littleEndian) ? 4 : 0;
+
+		return (this.getUint32(byteOffset + hi, littleEndian) << 32) |
+				this.getUint32(byteOffset + lo, littleEndian);
 	};
+}
 
-	return lc;
+
+function LibrecastCallback(obj, opcode, callback, temp) {
+	this.obj = obj;
+	this.opcode = opcode;
+	this.token = tok++;
+	this.temp = temp; /* delete this callback when done */
+	lcastCallbacks[this.token] = this;
+	if (typeof(callback) === 'function') {
+		this.callback = callback;
+	}
+}
+
+LibrecastCallback.prototype.call = function () {
+
+	if (typeof lcastCallbacks[this.token] === 'undefined') {
+		console.log("callback " + this.token + " undefined. Skipping.");
+		return false;
+	}
+	console.log("callback " + this.token + " triggered");
+	if (typeof(this.callback) === 'function') {
+		var args = [].slice.call(arguments);
+		args.unshift(this);
+		this.callback.apply(this, args);
+	}
+	if (this.temp) {
+		console.log("Deleting callback token " + this.token);
+		delete lcastCallbacks[this.token];
+	}
+};
+
+
+function LibrecastException(errorCode) {
+	this.code = errorCode;
+	this.name = librecastErrorMsg[errorCode];
+	this.errormsg = "ERROR (" + this.code + ") " + this.name;
+}
+
+
+function Librecast(onready) {
+	console.log("Librecast constructor");
+
+	this.url = (location.protocol == 'https:') ? "wss://" :  "ws://";
+	this.url += document.location.host + "/";
+
+	/* check for websocket browser support */
+	if (window.WebSocket) {
+		console.log("websockets supported");
+	}
+	else {
+		console.log("websockets unsupported");
+		throw new LibrecastException(LC_ERROR_WEBSOCKET_UNSUPPORTED);
+	}
+
+	this.id = undefined;
+
+	/* user callback functions */
+	this.onmessage = null;
+	this.onready = onready;
+
+	/* prepare websocket */
+	this.init();
+
+	this.defer = defer();
+}
+
+Librecast.prototype.init = function() {
+	console.log("Librecast.init()");
+	/* prepare websocket */
+	var self = this;
+	this.websocket = new WebSocket(this.url, "librecast");
+	this.websocket.binaryType = 'arraybuffer';
+	this.websocket.onclose = function(e) { self.wsClose(e); };
+	this.websocket.onerror = function(e) { self.wsError(e); };
+	this.websocket.onmessage = function(e) { self.wsMessage(e); };
+	this.websocket.onopen = function(e) { self.wsOpen(e); };
+};
+
+Librecast.prototype.close = function() {
+	console.log("Librecast close()");
+	this.websocket.close();
+};
+
+Librecast.prototype.wsClose = function(e) {
+	console.log("websocket close: (" + e.code + ") " + e.reason);
+	console.log("websocket.readyState: " + this.websocket.readyState);
+	console.log("reinitializing websocket");
+	this.init();
+};
+
+Librecast.prototype.wsError = function(e) {
+	console.log("websocket error" + e.message);
+	console.log("websocket.readyState: " + this.websocket.readyState);
+};
+
+Librecast.prototype.wsMessage = function(msg) {
+	console.log("websocket message received (type=" + msg.type +")");
+	var key, val;
+	if (typeof(msg) === 'object') {
+		if (msg.data instanceof ArrayBuffer) {
+			var dataview = new DataView(msg.data);
+			var opcode = dataview.getUint8(0);
+			var len = dataview.getUint32(1);
+			var id = dataview.getUint32(5);
+			var id2 = dataview.getUint32(9);
+			var token = dataview.getUint32(13);
+			var timestamp = dataview.getUint64(17) * 1000; /* s -> ms */
+
+			console.log("opcode: " + opcode);
+			console.log("len: " + len);
+			console.log("id: " + id);
+			console.log("id2: " + id2);
+			console.log("token: " + token);
+			console.log("timestamp: " + timestamp);
+			if (len > 0) {
+				if (opcode === LCAST_OP_CHANNEL_SETVAL) {
+					var keylen = dataview.getUint64(LCAST_HEADER_LENGTH);
+					key = new StringView(msg.data, "UTF-8", LCAST_HEADER_LENGTH + 8, keylen);
+					val = new StringView(msg.data, "UTF-8", LCAST_HEADER_LENGTH + 8 + keylen);
+				}
+				else {
+					var sv = new StringView(msg.data, "UTF-8", LCAST_HEADER_LENGTH, len);
+					val = sv.toString();
+				}
+			}
+			var cb = lcastCallbacks[token];
+			if (cb)
+				cb.call(opcode, len, id, token, key, val, timestamp);
+			else
+				console.log("message with no matching callback token '" + token + "'");
+		}
+	}
+	else if (typeof(this.onmessage) === 'function') {
+		//this.onmessage(msg); /* callback */
+		console.log(msg);
+	}
+	return defer();
+};
+
+Librecast.prototype.wsOpen = function(e) {
+	console.log("websocket open");
+	console.log("websocket.readyState: " + this.websocket.readyState);
+	if (this.defer) {
+		console.log("resolving Librecast.defer");
+		this.defer.resolve();
+	}
+	if (typeof(this.onready) === 'function') {
+		this.onready(); /* callback */
+	}
+};
+
+Librecast.prototype.send = function(obj, opcode, callback, data, len, temp) {
+	if (typeof len === 'undefined') { len = 0; }
+	var id = obj.id;
+	var id2 = obj.id2;
+	if (typeof obj.id === 'undefined') { id = 0; }
+	if (typeof obj.id2 === 'undefined') { id2 = 0;}
+	var cb = new LibrecastCallback(obj, opcode, callback, temp);
+	var buffer, dataview, idx;
+
+	console.log("sending message (" + opcode + ") with token '" + cb.token	+ "'");
+
+	if (typeof data === 'object') {
+		/* copy ArrayBuffer into new buffer with space for header data */
+		idx = LCAST_HEADER_LENGTH + len;
+		var tmp = new Uint8Array(idx);
+		tmp.set(new Uint8Array(data), LCAST_HEADER_LENGTH);
+		buffer = tmp.buffer;
+		dataview = new DataView(buffer);
+	}
+	else {
+		/* string data, convert to UTF-8 */
+		buffer = new ArrayBuffer(LCAST_HEADER_LENGTH + len * 4);
+		dataview = new DataView(buffer);
+		idx = convertUTF16toUTF8(LCAST_HEADER_LENGTH, data, len, dataview);
+	}
+
+	/* write headers */
+	dataview.setUint8(0, opcode);
+	dataview.setUint32(1, idx - LCAST_HEADER_LENGTH);
+	dataview.setUint32(5, id);
+	dataview.setUint32(9, id2);
+	dataview.setUint32(13, cb.token);
+
+	/* send */
+	this.websocket.send(buffer);
+};
+
+
+function LibrecastChannel(lctx, name, onready) {
+	console.log("Channel constructor");
+	this.lctx = lctx;
+	this.id = undefined;
+	this.ws = lctx.websocket;
+	this.name = name;
+	var cb = new LibrecastCallback(this, null, onready);
+	this.onready = cb;
+	lctx.send(this, LCAST_OP_CHANNEL_NEW, this.ready, name, name.length);
+
+	this.defer = defer();
+}
+
+LibrecastChannel.prototype.bind = function(sock, callback) {
+	console.log("binding channel " + this.name + " to socket " + sock.id);
+	this.id = this.id;
+	this.id2 = sock.id;
+	this.lctx.send(this, LCAST_OP_CHANNEL_BIND, callback);
+};
+
+LibrecastChannel.prototype.bound = function() {
+	console.log('bound channel');
+};
+
+LibrecastChannel.prototype.join = function(callback) {
+	console.log('joining channel "' + this.name + '"');
+	this.lctx.send(this, LCAST_OP_CHANNEL_JOIN, callback);
+};
+
+LibrecastChannel.prototype.joined = function() {
+	console.log('joined channel "' + this.name + '"');
+};
+
+LibrecastChannel.prototype.part = function() {
+	console.log('parting channel "' + this.name + '"');
+	this.lctx.send(this, LCAST_OP_CHANNEL_PART, this.parted);
+};
+
+LibrecastChannel.prototype.parted = function() {
+	console.log('parted channel "' + this.name + '"');
+};
+
+LibrecastChannel.prototype.ready = function(cb, opcode, len, id) {
+	console.log("callback with opcode " + cb.opcode + " and token " + cb.token);
+	console.log("setting channel id to " + id);
+
+	var self = cb.obj;
+	self.id = id;
+	if (self.defer) {
+		console.log("resolving Channel.defer");
+		self.defer.resolve();
+	}
+	self.onready.call();
+};
+
+LibrecastChannel.prototype.getmsg = function(cb) {
+	console.log("channel getmsgs");
+
+	/* TODO: filters (msgs since last received etc.) */
+
+	/* send query */
+	var key = "";
+	this.lctx.send(this, LCAST_OP_CHANNEL_GETMSG, cb, key, key.length);
+};
+
+LibrecastChannel.prototype.getval = function(key, cb) {
+	console.log("channel getval '" + key + "'");
+
+	/* set up callback, and send request */
+	this.lctx.send(this, LCAST_OP_CHANNEL_GETVAL, cb, key, key.length, true);
+};
+
+LibrecastChannel.prototype.setval = function(key, val, cb) {
+	console.log("channel setval '" + key + "': '" + val + "'");
+
+	var klen = key.length;
+	var vlen = val.length;
+	var buflen = 4 + (klen + vlen) * 4; /* allow up to 4 bytes per character */
+	var buffer = new ArrayBuffer(buflen);
+	var dataview = new DataView(buffer);
+	var idx = 4;
+
+	/* pack data and send */
+	/* [keylen][key][val] */
+	dataview.setUint32(0, klen);
+	idx = convertUTF16toUTF8(idx, key, klen, dataview);
+	idx = convertUTF16toUTF8(idx, val, vlen, dataview);
+	this.lctx.send(this, LCAST_OP_CHANNEL_SETVAL, cb, buffer, buflen, true);
+};
+
+LibrecastChannel.prototype.send = function(msg) {
+	if (this.lctx.websocket.readyState == WS_OPEN) {
+		console.log('sending on channel "' + this.name + '": ' + msg);
+		this.lctx.send(this, LCAST_OP_CHANNEL_SEND, null, msg, msg.length);
+	}
+	else {
+		throw new LibrecastException(LC_ERROR_WEBSOCKET_NOTREADY);
+	}
+};
+
+function LibrecastSocket(lctx, onready) {
+	console.log("Socket constructor");
+	this.lctx = lctx;
+	this.id = undefined;
+	var cb = new LibrecastCallback(this, null, onready, true);
+	this.onready = cb;
+	this.onmessage = undefined;
+	lctx.send(this, LCAST_OP_SOCKET_NEW, this.ready);
+	this.defer = defer();
+}
+
+LibrecastSocket.prototype.listen = function (callback) {
+	this.lctx.send(this, LCAST_OP_SOCKET_LISTEN, callback);
+};
+
+LibrecastSocket.prototype.ready = function (cb, opcode, len, id) {
+	console.log("Socket.ready()");
+
+	var self = cb.obj;
+	self.id = id;
+	if (self.defer) {
+		console.log("resolving Socket.defer");
+		self.defer.resolve();
+	}
+	self.onready.call();
+};
+
+return lc;
+
 }(jQuery));
