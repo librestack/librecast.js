a83c37e4756eaf471b1d3c6bcb55aae7ceca292f
Brett A C Sheffield <bacs@librecast.net>
2021-10-26 12:52:32 +0200

    refactor Socket

--- dist/librecast.js
+++ dist/librecast.js
@@ -257,15 +257,24 @@ lc.Socket = class {
 		});
 	};
 
+	op(opcode, data) {
+		return new Promise((resolve, reject) => {
+			if (this.lctx.websocket.readyState == lc.WS_OPEN) {
+				const msg = new lc.Message(data);
+				msg.opcode = opcode;
+				msg.id = this.id;
+				msg.token = this.lctx.callback(resolve, reject);
+				this.lctx.send(msg);
+			}
+			else {
+				reject(LibrecastException(lc.ERR_WEBSOCKET_NOTREADY));
+			}
+		});
+	}
+
 	listen() {
 		console.log("listening on socket " + this.id);
-		return new Promise((resolve, reject) => {
-			const msg = new lc.Message();
-			msg.opcode = lc.OP_SOCKET_LISTEN;
-			msg.id = this.id;
-			msg.token = this.lctx.callback(resolve, reject);
-			this.lctx.send(msg);
-		});
+		return this.op(lc.OP_SOCKET_LISTEN);
 	}
 };
 lc.Channel = class {
